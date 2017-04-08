3bfef9a1cdacd5cd00c7f65261705c541d98e711
Stéfano de Faria <stefanodefaria@gmail.com>
2017-04-08 14:26:36 +0000

    Fixed race condition that resulted in unnecessary authentication requests.

--- SwiftAuthenticator.js
+++ SwiftAuthenticator.js
@@ -1,44 +1,80 @@
 "use strict";
 
+const EventEmitter = require('events');
 const requestp = require('request-promise');
 
-/**
- * This module is designed to do a lazy authentication
- */
-class SwiftAuthenticator {
-    constructor(authUrl, username, password) {
-        // Values used for initial authentication
-        this.authUrl = authUrl;
-        this.username = username;
-        this.password = password;
+const AUTH_STATUS = {
+    UNAUTHENTICATED: 0,
+    AUTHENTICATED: 1,
+    FAILED: 2
+};
 
-        // Values used after authentication
-        this.authenticated = false;
-        this.url = null;
-        this.token = null;
+const AUTH_EVENT = 'authentication';
+
+class SwiftAuthenticator extends EventEmitter {
+    constructor(authUrl, username, password) {
+        super();
+
+        // Authenticated credentials
+        let url;
+        let token;
+
+        // Authentication process flags
+        let authStatus = AUTH_STATUS.UNAUTHENTICATED;
+        let authError = null;
+
+        // Starts authStatus process
+        requestp({
+            method: 'GET',
+            uri: authUrl,
+            headers: {
+                'x-auth-user': username,
+                'x-auth-key': password
+            },
+            resolveWithFullResponse: true
+        }).then(response => {
+            url = response.headers['x-storage-url'];
+            token = response.headers['x-auth-token'];
+            authStatus = AUTH_STATUS.AUTHENTICATED;
+            this.emit(AUTH_EVENT);
+        }).catch(err => {
+            authStatus = AUTH_STATUS.FAILED;
+            authError = err;
+            this.emit(AUTH_EVENT);
+        });
+
+        this._authenticate = function () {
+            let returnPromise;
+            switch (authStatus) {
+                case AUTH_STATUS.UNAUTHENTICATED:
+                    returnPromise = new Promise((resolve, reject) => {
+                        const authListener = () => {
+                            this.removeListener(AUTH_EVENT, authListener);
+                            if (authStatus === AUTH_STATUS.AUTHENTICATED) resolve({url: url, token: token});
+                            if (authStatus === AUTH_STATUS.FAILED) reject(authError);
+                        };
+
+                        this.on(AUTH_EVENT, authListener);
+                    });
+                    break;
+                case AUTH_STATUS.AUTHENTICATED:
+                    returnPromise = new Promise(resolve => {
+                        resolve({url: url, token: token});
+                    });
+                    break;
+                case AUTH_STATUS.FAILED:
+                    returnPromise = new Promise((resolve, reject) => {
+                        reject(authError);
+                    });
+                    break;
+            }
+
+            return returnPromise;
+        }
     }
 
-    authenticate(cb) {
-        if (this.authenticated) {
-            return new Promise(resolve => {
-                resolve({ url: this.url, token: this.token });
-            });
-        } else {
-            return requestp({
-                method: 'GET',
-                uri: this.authUrl,
-                headers: {
-                    'x-auth-user': this.username,
-                    'x-auth-key': this.password
-                },
-                resolveWithFullResponse: true
-            }).then(response => {
-                this.url = response.headers['x-storage-url'];
-                this.token = response.headers['x-auth-token'];
-                this.authenticated = true;
-                return { url: this.url, token: this.token };
-            });
-        }
+    authenticate() {
+        return this._authenticate();
     }
 }
 
